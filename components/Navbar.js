7b4c84353fd99533305795a08a156e567c5b229d
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-01-29 19:52:11 -0800

    Add dynamic route for video, update navbar with navlinks in data folder

---
+++ components/Navbar.js
@@ -0,0 +1,94 @@
+import React, { useState, useRef, useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { navLinks } from '../data/navlinks'
+import Hamburger from './Hamburger'
+
+export default function Navbar() {
+  const router = useRouter()
+  const linksRef = useRef(null)
+  const linksContainerRef = useRef(null)
+  const [open, setOpen] = useState(false)
+
+  const toggleDropdown = () => {
+    setOpen(!open)
+  }
+
+  // Hide dropdown if open is true and window is resized larger
+  useEffect(() => {
+    //Function to hide dropdown larger than 768px
+    const hideDropdown = () => {
+      if (window.innerWidth > 768 && open) {
+        setOpen(false)
+      }
+    }
+    //Add event listening on window resize
+    window.addEventListener('resize', hideDropdown)
+
+    //Event listener cleanup
+    ;() => {
+      window.removeEventListener('resize', hideDropdown)
+    }
+  })
+
+  useEffect(() => {
+    if (open) {
+      const linksHeight = linksRef.current.getBoundingClientRect().height
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } else {
+      linksContainerRef.current.style.height = '0px'
+    }
+  }, [open])
+
+  return (
+    <nav className="flex h-16 flex-col items-center justify-center border-b-2 border-b-black text-black">
+      <div className="container flex h-full items-center justify-between">
+        <Link href="/">
+          <a className="mr-4 inline-flex items-center p-2 ">
+            <span className="text-2xl font-bold uppercase tracking-wide text-black">
+              Sandals Church
+            </span>
+          </a>
+        </Link>
+
+        <div className="cursor-pointer px-4 md:hidden" onClick={toggleDropdown}>
+          <Hamburger />
+        </div>
+        <div className="hidden pr-8 md:block">
+          {navLinks.map(({ id, url, text }) => (
+            <Link href={url} key={id}>
+              <a
+                className={`p-4 hover:text-gray-600 ${
+                  router.asPath === url ? ' font-semibold ' : ''
+                } `}
+              >
+                {text}
+              </a>
+            </Link>
+          ))}
+        </div>
+      </div>
+      {/* DROPDOWN NAV */}
+      <div
+        className={`opacity-99 absolute top-16 z-10 h-0 w-full overflow-hidden rounded-b-xl bg-white text-center text-black  transition-all duration-300 ease-in-out  md:block`}
+        ref={linksContainerRef}
+      >
+        {/* hide links because they still show with height zero. */}
+        {open && (
+          <ul className={`flex flex-col`} ref={linksRef}>
+            {navLinks.map(({ id, url, text }) => (
+              <Link href={url} key={id}>
+                <a
+                  onClick={toggleDropdown}
+                  className="p-4 hover:bg-black hover:text-white"
+                >
+                  {text}
+                </a>
+              </Link>
+            ))}
+          </ul>
+        )}
+      </div>
+    </nav>
+  )
+}
---
+++ data/navlinks.js
@@ -0,0 +1,17 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/documentation',
+    text: 'Documentation',
+  },
+  {
+    id: 3,
+    url: '/contact',
+    text: 'Contact',
+  },
+]
